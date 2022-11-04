a0d73f0deb0f9ab7205475156231e8d3390d9cde
Mateo Algañarás <103078156+Tutealga@users.noreply.github.com>
2022-11-04 12:04:33 -0300

    Cambios mostrado productos

--- main.js
+++ main.js
@@ -5,44 +5,31 @@ const Contenedor = require('./container.js');
 const express = require('express');
 
 const productsTable = (products) => {
-    let productsTable = 
-    `<table border="1" align="center" bordercolor="black" cellspacing="2">
-        <caption style="padding: 20px; font-size:20px;"><B>Productos</B></caption>
-        <tr bgcolor="grey" align="center">
-            <th width=200>Titulo</th>
-            <th width=200>Precio</th>
-            <th width=200>Img</th>
-        </tr>`
-
-    products.forEach(product => {
-        productsTable = productsTable +
-        `<tr align="center">
-            <th width=200>${product.title}</td>
-            <th width=200>${product.price}</td>
-            <th width=200><img src="${product.thumbnail}" alt="${product.title}" width="200" height="200"/></td>
-        </tr>`
+    let productsTable = ``;
+  
+ products.forEach(product => {
+   productsTable = productsTable + 
+        `<div style="max-width: 200px; border-radius: .25rem; border:1px solid black">
+  <img style="width: 200px; height: 200px; border-top-right-radius:.25rem; border-top-left-radius:.25rem" src="${product.thumbnail}" alt="${product.title}">
+  <div style="padding:1rem; text-align:center">
+    <h3>${product.title}</h3>
+    <p>$${product.price}</p>
+  </div>
+</div>`
     });
-
-    productsTable = productsTable + `</table>`
 
     return productsTable;
 }
 
 const productTable = (product) => {
     const productTable = 
-    `<table border="1" align="center" bordercolor="black" cellspacing="2">
-        <caption style="padding: 20px; font-size:20px;"><B>Producto Random</B></caption>
-        <tr bgcolor="grey" align="center">
-            <th width=200>Titulo</th>
-            <th width=200>Precio</th>
-            <th width=200>Img</th>
-        </tr>
-        <tr align="center">
-            <th width=200>${product.title}</td>
-            <th width=200>${product.price}</td>
-            <th width=200><img src="${product.thumbnail}" alt="${product.title}" width="200" height="200"/></td>
-        </tr>
-    </table>`
+    `<div style="max-width: 200px; border-radius: .25rem; border:1px solid black">
+  <img style="width: 200px; height: 200px; border-top-right-radius:.25rem; border-top-left-radius:.25rem" src="${product.thumbnail}" alt="${product.title}">
+  <div style="padding:1rem; text-align:center">
+    <h3>${product.title}</h3>
+    <p>$${product.price}</p>
+  </div>
+</div>`
     return productTable;
 }
 
@@ -61,7 +48,7 @@ app.get('/', (req,res) => {
 app.get('/productos', async (req,res) => {
     const products = await container.getAll();
     let prodTable = productsTable(products);
-    res.send('<button><a href="/">Volver</a></button>' + prodTable);
+    res.send('<button><a href="/">Volver</a></button>' + `<div style="display:flex; gap:1rem"> ${prodTable} </div>`);
 });
 
 app.get('/productoRandom', async (req,res) => {
